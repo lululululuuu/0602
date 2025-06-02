4cfecf1ca61215911b7003e147b3d8e88f25959a
lululululuuu <147293556+lululululuuu@users.noreply.github.com>
2025-06-02 22:38:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,6 @@ const firebaseConfig = {
     measurementId: "G-PDS03JGPRR"
 };
 
-// 初始化 Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-const analytics = getAnalytics(app);
 
 // 頁面元素獲取
 const homePage = document.getElementById('homePage');
